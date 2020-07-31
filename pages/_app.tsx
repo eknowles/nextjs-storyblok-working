3db2083fe30f4dceda670f0ce81376bb67174931
Edward Knowles <ed.knowles@stunjelly.com>
2020-07-31 11:46:40 +0100

    fix line endings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,23 @@
-import React from 'react';import App from 'next/app';import { ThemeProvider } from '@chakra-ui/core';import theme from '../utils/theme';class MyApp extends App {  static async getInitialProps({ Component, ctx }) {    return {      pageProps: Component.getInitialProps ? await Component.getInitialProps(ctx) : {}    };  }  render() {    const { Component, pageProps } = this.props;    return (      <ThemeProvider theme={theme}>        <Component {...pageProps} />      </ThemeProvider>    );  }}export default MyApp;
+import React from 'react';
+import App from 'next/app';
+import { ThemeProvider } from '@chakra-ui/core';
+import theme from '../utils/theme';
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    return {
+      pageProps: Component.getInitialProps ? await Component.getInitialProps(ctx) : {}
+    };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
+
+export default MyApp;
